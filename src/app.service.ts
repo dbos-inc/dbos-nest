ace251e4368b368672c6aee1acc5218a0ce8fdc0
maxdml <maxdml@dbos.dev>
2024-11-03 13:19:27 -0800

    remove console log

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,6 @@ import { Workflow, WorkflowContext } from '@dbos-inc/dbos-sdk';
 export class AppService {
   @Workflow()  // Run this function as a database transaction
   static async getHello(ctxt: WorkflowContext) {
-    console.log('Hello from a wf');
     ctxt.logger.info('Hello from a wf');
     return 'Hello DBOS!';
   }
