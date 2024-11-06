dc10381d3aafc252c7a199237e945865f4a44f37
maxdml <maxdml@dbos.dev>
2024-11-06 11:22:08 -0800

    update with latest DBOS facade

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { DBOSService } from './dbos.service';
+import { DBOS } from '@dbos-inc/dbos-sdk';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly DBOSService: DBOSService) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   async getHello(): Promise<string> {
-    const dbos = this.DBOSService.getDBOSExecutor();
-    const response = await dbos.workflow(AppService.getHello, {});
+    const response = await DBOS.workflow(AppService.getHello, {});
     return response.getResult();
   }
 }
--- src/dbos.service.ts
+++ src/dbos.service.ts
@@ -1,34 +1,21 @@
 // dbos.service.ts
 import { Injectable } from '@nestjs/common';
-import {
-  DBOSExecutor,
-  DBOSConfig,
-  DBOSRuntimeConfig,
-  parseConfigFile,
-} from '@dbos-inc/dbos-sdk';
+import { DBOS } from '@dbos-inc/dbos-sdk';
 
 // This can be made more generic by using a custom DBOS event receiver?
 // Instead of manually returning the executor?
 @Injectable()
 export class DBOSService {
-  private dbos: DBOSExecutor | null = null;
-
   constructor() {
       console.log("built DBOSService");
   }
 
   async init() {
-    // Parse configuration and initialize DBOSExecutor
-    const [dbosConfig, _]: [DBOSConfig, DBOSRuntimeConfig] = parseConfigFile();
-    this.dbos = new DBOSExecutor(dbosConfig);
-    await this.dbos.init();
+    await DBOS.launch();
   }
 
-  getDBOSExecutor(): DBOSExecutor {
-    if (!this.dbos) {
-        throw new Error('DBOS Executor not initialized');
-    }
-    return this.dbos;
+  DBOS() {
+    return DBOS.executor;
   }
 }
 
